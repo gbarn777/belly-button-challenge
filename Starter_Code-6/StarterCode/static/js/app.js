a2c7425d91d5274b488a93259421449a0bed0eec
gracebarnett <gracebarnett288@gmail.com>
2023-04-04 12:30:49 -0400

    adding commands

---
+++ Starter_Code-6/StarterCode/static/js/app.js
@@ -0,0 +1,25 @@
+
+// Fetch data
+
+// Filter the data for the object with the desired sample number
+     
+// Use d3 to select the panel with id of `#sample-metadata`
+      
+// Use `.html("") to clear any existing metadata
+     
+// Hint: Inside the loop, you will need to use d3 to append new
+// tags for each key-value in the metadata.
+     
+// BONUS: Build the Gauge Chart
+
+// Build a Bubble Chart
+    
+// Grab a reference to the dropdown select element
+  
+// Use the list of sample names to populate the select options
+
+// Use the first sample from the list to build the initial plots
+    
+// Fetch new data each time a new sample is selected
+
+// Initialize the dashboard
